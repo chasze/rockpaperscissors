a55f469a5a8cc401afce1a65ef5ed958a6727b7e
Chase Dugan <chase@Chases-MacBook-Air.local>
2022-01-26 09:46:20 -0600

    rps with gui

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 
-    let choices = ['Rock', 'Paper', 'Scissors']
+
+    let rock = document.getElementById('rock')
+    let paper = document.getElementById('paper')
+    let scissors = document.getElementById('scissors')
+
+    let playerScore = 0;
+    let computerScore =0;
+
+    let headText = document.querySelector('h1')
+
+    let choiceText = document.querySelector('.choice')
+
+    let resultText = document.querySelector('.result')
+
+   
 
 function computerPlay () {
+    let choices = ['Rock', 'Paper', 'Scissors']
 
     let random  = Math.floor(Math.random() * 3)
        
        return choices[random]
 }
 
+function playGame(playerChoice) {
+
+    let computerChoice = computerPlay()
+    
+     choiceText.innerHTML = "Player chose: <div style='color: white;'>" + playerChoice + "</div>  Computer chose:  <div style='color: white;'>" + computerChoice + "</div>"
+ 
+     resultText.innerHTML = checkIfWon(playerChoice, computerChoice)
+
+     headText.innerHTML =  "Player: "  + playerScore + " Computer " + computerScore
+
+     if (playerScore == 5) {
+        headText.innerHTML = 'You Win!!'
+        playerScore = 0
+        computerScore =0
+     }
+     if (computerScore ==5) {
+        headText.innerHTML = 'You Lose! :('
+        playerScore = 0
+        computerScore =0
+
+     }
+
+
+
+ }
+ 
+
 function checkIfWon(player, computer) {
+
+
     if (player == 'Rock' && computer == 'Scissors') {
+        playerScore++
         return 'Player Wins!'
+
     }
     if (player == 'Scissors' && computer == 'Paper') {
+        playerScore++
         return 'Player Wins!'
     }
     if (player == 'Paper' && computer == 'Rock') {
+        playerScore++
         return 'Player Wins!'
     }
     if (player == 'Rock' && computer == 'Rock') {
@@ -31,24 +79,29 @@ function checkIfWon(player, computer) {
     }
 
     else {
+        computerScore++
         return "Computer Wins"
     }
     
-    
-
 }
 
 
-function playGame() {
-   let playerChoice = prompt("Choose Rock Paper or Scissors")
 
-   let computerChoice = computerPlay()
+rock.addEventListener( 'click', () => {
+  let  choice = 'Rock'
+    playGame(choice)
+})
 
-    console.log("Player chose: " + playerChoice + " Computer chose: " + computerChoice )
+paper.addEventListener('click', () => {
+  let  choice = 'Paper'
+      playGame(choice)
 
-    alert(checkIfWon(playerChoice, computerChoice))
+})
+
+scissors.addEventListener( 'click', () => {
+  let  choice = 'Scissors'
+      playGame(choice)
+})
 
 
-}
 
-playGame()
